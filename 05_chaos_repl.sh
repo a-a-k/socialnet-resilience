116c27e41e2666ef2d9c1df8f641ec1e150cda5e
Anatoly Krasnovsky <a.a.krasnovsky@gmail.com>
2025-04-27 18:19:55 +0300

    Update 05_chaos_repl.sh

--- 05_chaos_repl.sh
+++ 05_chaos_repl.sh
@@ -2,7 +2,7 @@
 set -euo pipefail; cd DeathStarBench/socialNetwork
 bash 00_helpers/just_kill.sh 0.30 & chaos=$!
 
-wrk2/scripts/wrk2/wrk -t2 -c64 -d60s -R300 \
+wrk2/wrk -t2 -c64 -d60s -R300 \
   -s scripts/social-network/mixed-workload.lua \
   http://localhost:8080/index.html > wrk_repl.log 2>&1
 kill $chaos || true
