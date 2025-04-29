51090cbe2deeee8bacd3af710918c95aa2d97dfe
Anatoly Krasnovsky <a.a.krasnovsky@gmail.com>
2025-04-29 12:41:55 +0300

    Update 04_steady_repl.sh

--- 04_steady_repl.sh
+++ 04_steady_repl.sh
@@ -7,9 +7,20 @@ docker compose up -d \
   --scale text-service=3 \
   --scale media-service=3
 
+# warm-up traffic (30 s, 300 RPS)
+echo "wrk ..."
+
+### data
+python3 scripts/init_social_graph.py --graph socfb-Reed98 --compose --ip 127.0.0.1 --port 8080
+echo "✅ data primed"
+
+echo "workload ..."
 wrk -t2 -c32 -d30s -R300 \
-  -s scripts/social-network/mixed-workload.lua \
+  -s wrk2/scripts/social-network/mixed-workload.lua \
   http://localhost:8080/index.html
+
+sleep 60
+echo "graph ..."
 
 ts=$(($(date +%s%N)/1000000))
 curl -s "http://localhost:16686/api/dependencies?endTs=$ts&lookback=3600000" \
