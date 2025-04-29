cc9652e768386fcd4c8845a4809b60ad7924b3e2
Anatoly Krasnovsky <a.a.krasnovsky@gmail.com>
2025-04-29 17:41:50 +0300

    Update 03_chaos_norepl.sh

--- 03_chaos_norepl.sh
+++ 03_chaos_norepl.sh
@@ -13,19 +13,24 @@ for r in $(seq 1 "$N"); do
         
   if [ -n "$ids" ]; then docker kill $ids || true; fi
 
+  echo "workload..."
   wrk -t2 -c32 -d30s -R300 \
   -s wrk2/scripts/social-network/mixed-workload.lua \
   http://localhost:8080/index.html > wrk1.log 2>&1
 
+  echo "workload done"
+  echo "gathering data..."
   errors=$(grep -Eo 'Non-2xx or 3xx responses:[[:space:]]*[0-9]+' wrk1.log |
            awk '{print $NF}' || echo 0)
   total=$(grep -Eo '[0-9]+ requests in' wrk1.log |
           awk '{print $1}' || echo 0)
 
+  echo "resulting json..."
   jq -n --argjson t "$total" --argjson e "$errors" --argjson n "$r" \
         '{round:$n,total:$t,errors:$e}' \
         >> results/chaos_runs.json
 
+  echo "done, restarting services..."
   docker compose restart $(docker compose config --services)
   sleep 40
 done
