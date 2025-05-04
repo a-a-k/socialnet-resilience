dde4103acd00566afc97861c007bbb062c6ad691
Alexander Zorkin <46386987+somnoynadno@users.noreply.github.com>
2025-05-04 08:50:38 +0300

    Activate python venv in pipeline_repl.sh

--- pipeline_repl.sh
+++ pipeline_repl.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 set -e
+
+if [ ! -d venv ]; then
+  echo "🔧  creating venv ..."
+  python3 -m venv venv
+  ./venv/bin/pip install -q --upgrade pip
+  ./venv/bin/pip install -q networkx numpy aiohttp
+fi
+
+source ./venv/bin/activate
+echo "✅  venv activated ($(python -V))"
+
 ./01_prepare_env.sh
 ./04_steady_repl.sh
 ./05_chaos_repl.sh
